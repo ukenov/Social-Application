92081170dc073d75cc235faed443bbedb64c5b2b
ukenov <ukenov.asylhan@gmail.com>
2022-11-23 02:21:34 +0600

    fix login error

--- social_frontend/src/components/Pin.jsx
+++ social_frontend/src/components/Pin.jsx
@@ -13,7 +13,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   const navigate = useNavigate();
 
   const user = fetchUser();
-  const alreadySaved = !!(save?.filter((item) => item.postedBy._id == user.sub))?.length;
+  const alreadySaved = !!(save?.filter((item) => item.postedBy._id == user?.sub))?.length;
 
   const savePin = (id) => {
     if(!alreadySaved) {
@@ -22,10 +22,10 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
         .setIfMissing({ save: [] })
         .insert('after', 'save[-1]', [{
           _key: uuidv4(),
-          userId: user.sub,
+          userId: user?.sub,
           postedBy: {
             _type: 'postedBy',
-            _ref: user.sub
+            _ref: user?.sub
           }
         }])
         .commit()
@@ -94,7 +94,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                     {destination.length > 20 ? destination.slice(8, 20) : destination.slice(8)}
                 </a>
               )}
-              {postedBy?._id === user.sub && (
+              {postedBy?._id === user?.sub && (
                 <button
                   type="button"
                   onClick={(e) => {
